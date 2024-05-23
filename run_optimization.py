dce74d86e421650a171a2705d77de16ec555ce13
vacany <32822411+vacany@users.noreply.github.com>
2024-05-23 12:56:20 +0200

    cuda

--- run_optimization.py
+++ run_optimization.py
@@ -77,6 +77,8 @@ for i, seq_id in tqdm(enumerate(seq_arrays[use_gpu]), total=len(seq_arrays[use_g
 
         p1, p2, c1, c2, f1 = let_it_flow.initial_clustering(global_list, frame, TEMPORAL_RANGE, device, eps=eps, min_samples=min_samples, z_scale=0.5)
 
+        print(p1.device)
+        
         optimizer = torch.optim.Adam([f1], lr=lr)
 
         RigidLoss = let_it_flow.SC2_KNN_cluster_aware(p1, K=K, d_thre=d_thre)
@@ -105,7 +107,7 @@ for i, seq_id in tqdm(enumerate(seq_arrays[use_gpu]), total=len(seq_arrays[use_g
             optimizer.step()
             optimizer.zero_grad()
 
-    print(p1.device)
+
         
     elif model == 'chodosh':
         f1 = infer_chodosh(p1[0], p1[0], p2[0], max_iters=cfg['iters']).unsqueeze(0)
