6f1d32be8bb42d423d939aeb4353ed08f21baefc
vacany <32822411+vacany@users.noreply.github.com>
2024-05-23 14:08:59 +0200

    computation of metric

--- tools/sbatch_run.sh
+++ tools/sbatch_run.sh
@@ -6,17 +6,19 @@
 #SBATCH --partition=amdgpufast      # partition name
 #SBATCH --gres=gpu:1                 # 1 gpu per node
 #SBATCH --mem=30G
-#SBATCH --error=log/ST_abla_without_K.err            # standard error file
-#SBATCH --output=log/ST_abla_without_K.out           # standard output file
-#SBATCH --array 0-3%4
+#SBATCH --error=datasets/log.out            # standard error file
+#SBATCH --output=datasets/log.out           # standard output file
+
+######------#SBATCH --array 0-3%4
 
 ml PyTorch3D/0.7.5-foss-2023a-CUDA-12.1.1
-ml scikit-learn/1.3.1-gfbf-2023a
+ml kornia/0.7.2-foss-2023a-CUDA-12.1.1
+ml OpenCV/4.8.1-foss-2023a-CUDA-12.1.1-contrib
 ml JupyterLab/4.0.5-GCCcore-12.3.0
-
 
 
 cd ~/let-it-flow/
 
+python run_optimization.py
 
-python run_exps.py ${SLURM_ARRAY_TASK_ID} 4 # 4 is the number of runs per experiment and indexing is minus 1 basially
+# python run_optimization.py ${SLURM_ARRAY_TASK_ID} 4 # 4 is the number of runs per experiment and indexing is minus 1 basially
